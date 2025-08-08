f6151c10f4cc1afd49fcd479f8c53442babf8c66
Parham <prmbas@gmail.com>
2025-08-08 21:42:55 +0000

    output_filename form yaml

--- src/jay.py
+++ src/jay.py
@@ -187,6 +187,6 @@ if __name__ == '__main__':
     print(f'[*] Function applied: {m}')
 
     # Save result to CSV
-    output_path: str = f'../data/{configs["name"]}.csv'
+    output_path: str = f'{configs['function']}/{configs["name"]}.csv'
     m.data.to_csv(output_path, index = False)
     print(f"[*] Data stored: `{output_path}`")
